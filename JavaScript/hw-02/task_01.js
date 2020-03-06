97025428d09f43e7877c85ffce100cf38de1590c
vitalii <56398871+vitaliidombrovskii@users.noreply.github.com>
2020-03-06 23:07:08 +0200

    task_07

--- JavaScript/hw-02/task_01.js
+++ JavaScript/hw-02/task_01.js
@@ -7,16 +7,14 @@
 
 'use strict';
 
-const logItems = ['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']
-
-for (let i = 0; i < logItems.length; i += 1)
-
-  console.log(`${i+1} - ${logItems[i]}`);
-
-
+const logItems = function(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+};
 /*
  * Вызовы функции для проверки работоспособности твоей реализации.
  */
-// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
 
-// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
